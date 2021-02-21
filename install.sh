3eb9909dce682d591e0901a94a03bf344c9d57d2
Divyanshu Sharma <70474633+divu050704@users.noreply.github.com>
2021-02-21 21:50:08 +0530

    Removed too many bugs

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 echo -e "\e[1;35mInstalling...... \e[0m"
-sudo mv whatsc-main /usr/share/.whatsc-main && sudo mv whatsapp.desktop /usr/share/applications && cp /usr/share/applications/whatsapp.desktop ~/Desktop 
+sudo mv whatsc-main /usr/.whatsc-main && sudo mv whatsapp.desktop /usr/share/applications && cp /usr/share/applications/whatsapp.desktop ~/Desktop && sudo chmod +x /usr/.whatsc-main/whatsapp.sh
 echo -e "\e[1;35mInstalled\e[0m"
